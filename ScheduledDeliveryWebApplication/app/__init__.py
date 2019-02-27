f562d10baede3b810b382d0fdecec85feeb51384
Lucas Leitão Cândido da Silva <leitao.bcc@gmail.com>
2019-02-27 11:55:48 -0300

    configuration for running on docker

--- ScheduledDeliveryWebApplication/app/__init__.py
+++ ScheduledDeliveryWebApplication/app/__init__.py
@@ -9,21 +9,29 @@ from app.resources.merchant_resource import MerchantResource
 from app.resources.order_resource import OrderResource
 from config import Config
 
-app = Flask(__name__)
-
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-
-migrate = Migrate(app, db)
-
-api = Api(app)
-
-api.add_resource(CatalogResource, '/catalog')
-api.add_resource(MerchantResource, '/merchant/<string:merchant_id>')
-api.add_resource(AuthResource, '/auth/login')
-api.add_resource(OrderResource, '/order/<string:order_id>')
+db = SQLAlchemy()
+migrate = Migrate()
+api = Api()
 
 from app.models import address_model, category_model, customer_model, \
     item_model, merchant_model, office_hour_model, order_model, order_item, \
     phone_model
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    migrate.init_app(app, db)
+
+    api.init_app(app)
+
+    api.add_resource(CatalogResource, '/catalog')
+    api.add_resource(MerchantResource, '/merchant/<string:merchant_id>')
+    api.add_resource(AuthResource, '/auth/login')
+    api.add_resource(OrderResource, '/order/<string:order_id>')
+
+    return app
